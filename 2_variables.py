8d28d0d00e9dd52ae53e6bbad615eb97a4caa23c
Mohamed-Adel <mohamedadel662001200@gmail.com>
2024-07-23 15:00:38 +0300

    solved 1: variables

---
+++ 2_variables.py
@@ -0,0 +1,34 @@
+# todo:1 Create a variable called break and assign it a value 5.\
+# See what happens and find out the reason behind the behavior that you see.
+
+# break = 5
+
+# todo:2 Create two variables. One to store your birth year and another one to store current year.\
+# Now calculate your age using these two variables
+
+birth_year = 2001
+current_year = 2024
+print("My age is", current_year - birth_year)
+
+# todo:3 Store your first, middle and last name in three different variables and then print your full name using these variables
+
+first_name = "Mohamed"
+middle_name = "Adel"
+last_name = "Mohamed"
+
+print(first_name, middle_name, last_name)
+
+# todo:4 Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+names = ["_nation", "1record", "record1", "record_one", "record-one", "record^one", "continue"]
+valid_names = []    # valid names [_nation, record1, record_one]
+invalid_names = []  # invalid names [1record, record-one, record^one, continue]
+
+for name in names:
+    try:
+        exec(f"{name} = 1")
+        valid_names.append(name)
+    except SyntaxError:
+        invalid_names.append(name)
+
+print("Valid names:", valid_names)
+print("Invalid names:", invalid_names)
